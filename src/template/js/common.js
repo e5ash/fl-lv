ea4dc98c045fe1bbf640caf7102636fdeec38a73
e5ash <e5ash.bro@gmail.com>
2021-10-04 16:03:39 +0400

    full

--- src/template/js/common.js
+++ src/template/js/common.js
@@ -2,42 +2,32 @@ var html = document.querySelector('html'),
 		body = document.querySelector('body'),
 		wrap = document.querySelector('.wrap');
 
-document.addEventListener('DOMContentLoaded', ()=>{
-	// Fields
-	let fields = document.querySelectorAll('.field');
-	 
-	if (fields) {
-		fields.forEach((field)=>{
-			new Field(field);
-		});
+new fullpage('#full_page', {
+	//options here
+	menu: '#nav',
+	autoScrolling:true,
+	scrollHorizontally: true,
+	navigation: true,
+	navigationPosition: 'left',
+	showActiveTooltip: true,
+	navigationTooltips: ['', 'Mission', 'Technologies', 'Products', 'Let\'s connect'],
+	responsiveWidth: 768,
+	responsiveHeight: 650,
+});
+
+let nav = document.querySelector('.header__nav'),
+		ham = document.querySelector('.header__ham'),
+		classToggle = '--toggle';
+
+
+ham.addEventListener('click', ()=>{
+	ham.classList.toggle(classToggle);
+	nav.classList.toggle(classToggle);
+});
+
+nav.addEventListener('click', (event)=>{
+	if (event.target.closest('li')) {
+		ham.classList.remove(classToggle);
+		nav.classList.remove(classToggle);
 	}
-
-
-	// Checks
-	let checks = document.querySelectorAll('.check');
-	 
-	if (checks) {
-		checks.forEach((check)=>{
-			new Check(check);
-		});
-	}
-
-
-	// Selects
-	var selects = document.querySelectorAll('.select');
-	if (selects) {
-			selects.forEach(select => {
-		  new Select(select);
-		});
-
-		document.addEventListener('click', (event)=>{
-			let openSelects = document.querySelectorAll('.select.--open');
-			if (!event.target.closest('.select') && openSelects) {
-				openSelects.forEach((select)=> {
-					select.classList.remove(Select.classOpen);
-				});
-			}
-		})
-	}
-
 });
